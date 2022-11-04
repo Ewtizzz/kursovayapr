691145f2db15e39035d93600f449a4320a04cdf9
Egor <egor.zholobov@gmail.com>
2022-11-04 13:57:17 +0300

    сделано

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.lang.reflect.Array;
+
 public class Main {
+    public static void isPalindrome(String words){
+        if ((words.replaceAll("\\W","")).equalsIgnoreCase(new StringBuilder(words).reverse().toString().replaceAll("\\W",""))){
+            System.out.println("Строчка является палиндромом");
+        } else {
+            System.out.println("Строчка не является палиндромом");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        isPalindrome("А роза упала на лапу азора");
     }
 }
